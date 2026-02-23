694ea503bfaaf9e62b6d778781a91158ca023537
mrtrader131 <msrabeyakhatun47@gmail.com>
2026-02-23 07:46:35 -0800

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,14 +1,21 @@
 export default async function handler(req, res) {
-  const API_KEY = "275ef6d202-8c3ae13696-taure7";
-  const { from, to } = req.query;
+  try {
+    const API_KEY = "তোমার_API_KEY_এখানে";275ef6d202-8c3ae13696-taure7
+    const { from, to } = req.query;
 
-  const response = await fetch(
-    https://api.fastforex.io/fetch-one?from=${from}&to=${to},
-    {
-      headers: { "X-API-Key": API_KEY }
-    }
-  );
+    const response = await fetch(
+      https://api.fastforex.io/fetch-one?from=${from}&to=${to},
+      {
+        headers: {
+          "X-API-Key": API_KEY
+        }
+      }
+    );
 
-  const data = await response.json();
-  res.status(200).json(data);
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
